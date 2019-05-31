55e5bfb7340d71596c2e70e9faf163e78de447df
Kamil Turek <241277@student.pwr.edu.pl>
2019-05-31 19:11:02 +0200

    Tiding up

--- tests/tests.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-extern "C" const char * addition(char, const char *, const char *);
-
-int main(int argc, char const *argv[])
-{
-  std::cout << addition('b', "100\n", "1000\n");
-  return 0;
-}
